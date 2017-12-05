129352a0bc78536cb207b65d370c67875c1831ce
zhao <zhaoyudong_0129@126.com>
2017-12-05 18:49:14 +0800

    add proxy to test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 var page = require('webpage').create();
+
+phantom.setProxy('proxy.crawlera.com', '8010', 'http'); // change to https when making HTTPS requests
+page.customHeaders={'Proxy-Authorization': 'Basic '+btoa('5dba0f67463b4e828e803497d8c1b985:')}; // Make sure to include ':' at the end
+
 page.open('https://www.youtube.com/user/PhoneBunch/about', function(status) {
   console.log("Status: " + status);
   if(status === "success") {
