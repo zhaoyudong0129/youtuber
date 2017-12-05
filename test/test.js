aaf675953d8e394fb77545810c43f563e5877ed1
zhao <zhaoyudong_0129@126.com>
2017-12-05 18:54:17 +0800

    change test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,16 @@
 var page = require('webpage').create();
 
 phantom.setProxy('proxy.crawlera.com', '8010', 'http'); // change to https when making HTTPS requests
-page.customHeaders={'Proxy-Authorization': 'Basic '+btoa('5dba0f67463b4e828e803497d8c1b985:')}; // Make sure to include ':' at the end
+page.customHeaders = {'Proxy-Authorization': 'Basic ' + btoa('5dba0f67463b4e828e803497d8c1b985:')}; // Make sure to include ':' at the end
 
-page.open('https://www.youtube.com/user/PhoneBunch/about', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('example.png');
-  }
-  phantom.exit();
+page.open('http://httpbin.org/ip', function (status) {
+    console.log("Status: " + status);
+    if (status === "success") {
+        page.evaluate(function () {
+        });
+        console.log(page.content);
+
+        phantom.exit();
+    }
+    phantom.exit();
 });
